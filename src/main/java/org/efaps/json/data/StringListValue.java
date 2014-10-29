cc19513f1303aec80c9e9304ba53c25aaa57d371
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-10-29 20:05:32 +0000

    - JSON: adding datatype
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-JSON/trunk@14322 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/java/org/efaps/json/data/StringListValue.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.json.data;
+
+import java.util.List;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+public class StringListValue
+    extends AbstractValue<List<String>>
+{
+
+    /**
+     * List of values.
+     */
+    private List<String> valueList;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> getValue()
+    {
+        return this.valueList;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public AbstractValue<List<String>> setValue(final List<String> _value)
+    {
+        this.valueList = _value;
+        return this;
+    }
+
+}
