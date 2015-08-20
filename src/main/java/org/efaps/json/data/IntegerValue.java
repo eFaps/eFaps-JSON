426d8f1653fd3f314c9425e404e585374a490917
Jan Moxter <jan.moxter@innobix.com>
2015-08-20 02:29:09 -0500

    - Issue #1: Add IntegerValue
    closes #1

---
+++ src/main/java/org/efaps/json/data/IntegerValue.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2003 - 2015 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.efaps.json.data;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ */
+public class IntegerValue
+    extends AbstractValue<Integer>
+{
+
+    private Integer value;
+
+    /**
+     * Getter method for the instance variable {@link #value}.
+     *
+     * @return value of instance variable {@link #value}
+     */
+    @Override
+    public Integer getValue()
+    {
+        return this.value;
+    }
+
+    /**
+     * Setter method for instance variable {@link #value}.
+     *
+     * @param _value value for instance variable {@link #value}
+     */
+    @Override
+    public IntegerValue setValue(final Integer _value)
+    {
+        this.value = _value;
+        return this;
+    }
+}
